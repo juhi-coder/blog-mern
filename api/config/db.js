42b39ff0589c86a5f3a87e6e4c1eb39e9903866d
juhi-coder <kumarijuhimdb2@gmail.com>
2024-06-18 21:31:47 +0530

    add

--- api/config/db.js
+++ api/config/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 async function dbConnection() {
   try {
-    await mongoose.connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB_URL);
     console.log("Database successfully connected");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
